e6657d1eb4dc9402e961223052bad42a9f978bcc
Natan Ciupek <nano888@interia.eu>
2021-02-10 14:13:16 +0100

    update css, and classes Cell, Game, UI and add two classes Counter and Timer

---
+++ js/Timer.js
@@ -0,0 +1,32 @@
+import {UI} from './UI.js'
+
+export class Timer extends UI {
+    #element = null;
+    #interval = null;
+    numberOfSeconds = 0;
+    #maxNumberOfSeconds = 999;
+
+    init() {
+        this.#element = this.getElement(this.UiSelectors.timer);
+    }
+
+    startTimer() {
+        setInterval(() => {
+            this.#updateTimer()
+        },1000);
+    }
+
+    stopTimer() {
+        this.#interval = clearInterval(this.#interval);
+    }
+
+    #updateTimer() {
+        this.numberOfSeconds++;
+        this.numberOfSeconds <= this.#maxNumberOfSeconds ? this.#setTimerValue(this.numberOfSeconds) : this.stopTimer();
+
+    }
+
+    #setTimerValue(value) {
+        this.#element.textContent = value;
+    }
+}
